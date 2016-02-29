7589591bf7d1f209e6f9d8a1b3164f9195017261
demun <hjm01@naver.com>
2016-02-29 11:16:57 +0900

    test files others pull

--- _sass/config.rb
+++ _sass/config.rb
@@ -1,7 +1,7 @@
 http_path = "/"
 css_dir = "."
-sass_dir = "../_sass"
-images_dir = "img"
+sass_dir = "_sass"
+images_dir = "images"
 javascripts_dir = "js"
 output_style = :compressed
 relative_assets = true
